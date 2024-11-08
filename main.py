dfe016cdcb1d23b1d302f738f9f019a5477e1061
Programmer Sudip <sudipmicrosoft259@gmail.com>
2024-11-08 22:34:43 +0600

    list

---
+++ main.py
@@ -0,0 +1,37 @@
+# Program to showcase working of different types of bitwise operators
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+# Using AND operator
+print("num1 & num2 = ", num1 & num2)
+
+# Using OR operator
+print("\nnum1 | num2 = ", num1 | num2)
+
+# Using NOT operator
+print("\n~num1 = ", ~num1)
+
+# Using XOR operator
+print("\nnum1 ^ num2 = ", num1 ^ num2)
+
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+
+# Using right shift operator on num1
+print("\nnum1 >> 1 = ", num1 >> 1)
+
+# Using right shift operator on num2
+print("\nnum2 >> 1 = ", num2 >> 1)
+
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter another number: "))
+
+
+# Using left shift operator on num1
+print("\nnum1 << 1 = ", num1 << 1)
+
+# Using left shift operator on num2
+print("\nnum2 << 1 = ", num2 << 1)
